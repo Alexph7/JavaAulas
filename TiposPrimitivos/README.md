b353846283dd710c5a7e15e44df4ebf5aec42934
Alexph7 <rochaealexrodrigues@gmail.com>
2020-12-17 17:58:55 -0300

    Arquivos Criados

---
+++ TiposPrimitivos/README.md
@@ -0,0 +1,2 @@
+# JavaInicio
+Primeiros Passos Java
---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,44 @@
+
+package tiposprimitivos;
+        //Arquivo para texto
+        
+    /*          3 FORMAS DE DECLARAR VARIÁVEIS
+    *1 Normal
+        *int idade = 30;
+        *float sal = 123.34f;
+        *char letra = 'G';
+        *boolean casado = false;
+        *
+    *2 TYPECAST
+        *int idade = (int) 30; 
+        *float sal = (float) 123.34;
+        *char letra = (char) 'G';
+        *boolean casado = (boolean) false;
+        *
+    *3 WRAPPER CLASS (são Classes, não é variavél e sim um objeto)
+        *
+        *Integer idade = new Integer(30); 
+        *Float sal = new Float(123.34)
+        *Character letra = new Character('G');
+        *Boolean casado = new Boolean(false);
+        
+        
+        FAMILIA      TIPO        CLASSE       TAMANHO       EXEMPLO
+                   PRIMITIVO    INVÓLUCRO
+        Lógico      boolean      Boolean       1 bit          true
+        
+        Literais     char       Character      1 byte         'A'
+        Literais      --         String      1 byte/cada     "JAVA"
+        
+        Inteiros     byte        Byte          1 byte          127
+        Inteiros     short       Short         2 bytes        32 767
+        Inteiros     int         Integer       4 bytes       2 147 483
+        Inteiros     long        Long          8 bytes     2 (elevado 63)
+        
+        Reais        float       Float         4 bytes     3.4e(elevado +38)
+        Reais        double      Double        8 bytes     1.8e(elevado +308)
+
+        */    
+    }
+    
+}
