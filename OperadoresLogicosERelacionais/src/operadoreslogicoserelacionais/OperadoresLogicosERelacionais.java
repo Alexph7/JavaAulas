b7dd44a06524598aebf62f145f2b7be2329ca1d3
Alexph7 <rochaealexrodrigues@gmail.com>
2020-12-18 14:42:36 -0300

    Operação 1

--- OperadoresLogicosERelacionais/src/operadoreslogicoserelacionais/OperadoresLogicosERelacionais.java
+++ OperadoresLogicosERelacionais/src/operadoreslogicoserelacionais/OperadoresLogicosERelacionais.java
@@ -7,7 +7,13 @@ package operadoreslogicoserelacionais;
 public class OperadoresLogicosERelacionais {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Operador Ternário
+        int n1, n2, r;
+        n1=14;
+        n2=18;
+        r=(n1>n2)?n1:n2;
+        System.out.println(r);
+
     }
     
 }
