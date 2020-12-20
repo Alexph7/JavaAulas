3de3cdd1370777405d079c32aa8266f204578f29
Alexph7 <rochaealexrodrigues@gmail.com>
2020-12-20 12:20:39 -0300

    Estrutura Repetição arquivo Criado

---
+++ EstruturaRepeticao/src/estruturarepeticao/EstruturaRepeticao.java
@@ -0,0 +1,19 @@
+package estruturarepeticao;
+
+/**
+ * @author ALEX
+ */
+public class EstruturaRepeticao {
+    
+    public static void main(String[] args) {
+        int i=0;
+        while (i<10){
+            i++;
+            System.out.println("Cambalhota "+i);
+            
+        }
+        
+        
+    }
+    
+}
