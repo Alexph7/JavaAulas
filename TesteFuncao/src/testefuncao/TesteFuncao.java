cfac5812c547abb937346b2fa3aa7a9dcaedc532
Alexph7 <rochaealexrodrigues@gmail.com>
2020-12-22 15:14:24 -0300

    Metodos

---
+++ TesteFuncao/src/testefuncao/TesteFuncao.java
@@ -0,0 +1,21 @@
+package testefuncao;
+
+/**
+ * @author ALEX
+ */
+public class TesteFuncao {
+
+    /**
+     * @param args the command line arguments
+     */
+static int soma (int a, int b){
+    int s = a+b;
+    return s;
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sm = soma(5,2);
+        System.out.println(sm);
+    }
+    
+}
