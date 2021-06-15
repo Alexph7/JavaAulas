de95c181510c3c225b1ab59735acc6020dd08071
Alex Rodrigues <73371259+Alexph7@users.noreply.github.com>
2021-06-15 19:12:53 -0300

    Update 2. matrizBiDimensionais.java

--- Arrays-Matriz/2. matrizBiDimensionais.java
+++ Arrays-Matriz/2. matrizBiDimensionais.java
@@ -1,16 +1,10 @@
 Declarando Arrays(vetores) 2 Dimensões em Java
 //Geralmente usados em tabelas
-1.    int[][] idade = new int[3][4];
+int[][] idade = new int[3][4];
 
 //ou:
 //Declarando um array bidimensional 2 por 4 (2x4 = 8 elementos)
-2.    int[][] idade = { { 0, 0, 0, 0 },{ 0, 0, 0, 0 },{ 0, 0, 0, 0 }};
-
-//Você também pode ter colunas diferentes em linhas diferentes. Por exemplo:
-3.    int[][] idade = new int[5][]; 
-idade[0] = new int[10]; 
-idade[1] = new int[6]; 
-idade[2] = new int[9]
+int[][] idade = { { 0, 0, 0, 0 },{ 0, 0, 0, 0 },{ 0, 0, 0, 0 }};
 
 //Lendo o Array
 for(int i=0; i<idade.lenght; i++){
