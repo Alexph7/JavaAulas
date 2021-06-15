f40553bf4d394c5a5cd682a591b08c87c058ca7f
Alex Rodrigues <73371259+Alexph7@users.noreply.github.com>
2021-06-15 17:50:22 -0300

    Update and rename Arrays1.java to ArraysSimples.java

---
+++ Arrays1/src/arrays1/ArraysSimples.java
@@ -0,0 +1,15 @@
+Declarando Arrays(vetores) em Java
+//A sintaxe da declaração e criação do array é a seguinte:
+//tipo[] nome_do_array = new tipo[numero_de_elementos];
+int[] idade = new idade[4];
+
+//Ou:
+//tipo[] nome_do_array = { valor1, valor2, ...,valorx};
+int[] idade = {13,24,87,34};
+
+//Lendo o Array
+for(int i=0; i<idade.lenght; i++){
+    
+}
+===================================================================================================================================================================================
+    
