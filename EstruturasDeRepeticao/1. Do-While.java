ccd89c8b73a2e184bfed2ecb2acba9c7fa193f6c
Alex Rodrigues <73371259+Alexph7@users.noreply.github.com>
2021-06-16 18:25:31 -0300

    Update and rename AulaEstruturaRepeticao/src/estruturarepeticao/EstruturaRepeticao.java to EstruturasDeRepeticao/1. Do-While.java

---
+++ EstruturasDeRepeticao/1. Do-While.java
@@ -0,0 +1,23 @@
+    int i = 0;
+
+    do{
+     //código que será executado.
+    } while(true);
+  
+========================
+    
+public class DoWhileExemplo {  
+    public static void main(String[] args) {  
+   
+        int i = 1;  
+        
+        do {  
+              System.out.println(i);  
+              i++;  
+           }while(i <= 10);  
+    }  
+}  
+
+=========================
+    
+    
