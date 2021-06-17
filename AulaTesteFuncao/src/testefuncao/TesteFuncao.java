807cf0b75b7ac753c31b4ba177bfcd16ca9e3fd1
Alex Rodrigues <73371259+Alexph7@users.noreply.github.com>
2021-06-17 12:47:25 -0300

    Update TesteFuncao.java

--- AulaTesteFuncao/src/testefuncao/TesteFuncao.java
+++ AulaTesteFuncao/src/testefuncao/TesteFuncao.java
@@ -1,17 +1,9 @@
-package testefuncao;
 
-/**
- * @author ALEX
- */
-public class TesteFuncao {
-
-    /**
-     * @param args the command line arguments
-     */
 static int soma (int a, int b){
     int s = a+b;
     return s;
 }
+
     public static void main(String[] args) {
         // TODO code application logic here
         int sm = soma(5,2);
