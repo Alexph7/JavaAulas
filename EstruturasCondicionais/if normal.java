d69497795652471037aecac1e739298df6ecdad3
Alex Rodrigues <73371259+Alexph7@users.noreply.github.com>
2021-06-17 00:18:59 -0300

    Update and rename AulaEstruturasCondicionais/src/estruturascondicionais/EstruturasCondicionais.java to EstruturasCondicionais/if normal.java

---
+++ EstruturasCondicionais/if normal.java
@@ -0,0 +1,24 @@
+
+if(condicao) {  
+   
+    //código que será executado  
+
+}   
+
+===========================================
+    
+Exemplo:
+
+//Programa java para demonstrar o uso da declaração if  
+
+public class IfExemplo{  
+   public static void main(String[] args) {  
+
+       //definindo uma variável 'idade'  
+       int idade = 20;  
+       
+       //checando a idade  
+       if(idade > 18) {  
+         System.out.println("Idade e maior que 18");       
+      }  
+   }  
